e6cfa7dc78dea4e25aacd2b32566d2837bbbf8c5
Andela Development <oluwagbenga.joloko@andela.com>
2017-04-27 11:51:50 +0100

    update test

--- test/Header-test.js
+++ test/Header-test.js
@@ -7,12 +7,4 @@ describe("test for header component", function() {
   it("should contain a nav component", function() {
     expect(shallow(<Header />).contains(<nav/>)).to.exist;
   });
-  it('should render children when passed in', () => {
-    const wrapper = shallow(
-      <Header>
-        <div className="unique" >hi</div>
-      </Header>
-    );
-    expect(wrapper.contains(<div className="unique" >hi</div>)).to.equal(true);
-  });
 });
